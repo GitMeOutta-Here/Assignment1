7cfc51c2cfd8e8c095045d4c0ff6518b2a3e5e85
JackG <Jackgrossenbacher@gmail.com>
2026-07-13 17:26:49 -0500

    Committed InventoryManger class to feature-display branch with printInventory method

---
+++ InventoryManager.java
@@ -0,0 +1,18 @@
+public class InventoryManager {
+    public static void printInventory(String[] names, double[] prices, int[] stocks) {
+        for (int i = 0; i < 10; i++) {
+            if (names[i] == null || prices[i] == 0) {
+                System.out.print("");
+            }
+            else {
+            System.out.println("#####################");
+            System.out.println("Item Name: " + names[i]);
+            System.out.println("Item Price: " + prices[i]);
+            System.out.println("Item Stock: " + stocks[i]);
+            }
+            if (i == 9) {
+                System.out.println("#####################");
+            }
+        }
+    }
+}
