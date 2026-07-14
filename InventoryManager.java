600f144d9339e41d8b41e108bf109e9edd7cb215
JackG <Jackgrossenbacher@gmail.com>
2026-07-13 19:24:07 -0500

    Completed Task 3 (main method with scanner loop), and committed to branch 'feature-menu'

--- InventoryManager.java
+++ InventoryManager.java
@@ -1,4 +1,48 @@
+import java.util.Scanner;
+
 public class InventoryManager {
+    public static void main(String[] args) {
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+
+        System.out.println("Welcome to the Inventory Manager!\n");
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("Please make a selection: ");
+                System.out.print("1 to view inventory, 2 to restock an item, 3 to exit: ");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Invalid selection, please enter 1, 2, or 3.");
+                    scanner.nextLine();
+                }
+                int selection = scanner.nextInt();
+                switch (selection) {
+                    case 1 -> printInventory(itemNames, itemPrices, itemStocks);
+                    case 2 -> {
+                        System.out.print("Which item are you restocking?: ");
+                        while (scanner.hasNextInt() || scanner.hasNextDouble()) {
+                            System.out.println("Invalid input. Enter the name of the item you are restocking.");
+                            scanner.nextLine();
+                        }       String itemToRestock = scanner.nextLine();
+                        System.out.print("How many of that item are you restocking?: ");
+                        while (!scanner.hasNextInt()) {
+                            System.out.println("Invalid selection. Please enter a whole number.");
+                        }       int numToRestock = scanner.nextInt();
+                        restockItem(itemNames, itemStocks, itemToRestock, numToRestock);
+                    }
+                    case 3 -> {
+                        System.out.println("Program exiting. Goodbye.");
+                        return;
+                    }
+                    default -> {
+                        System.out.println("Invalid Selection. Please try again.\n");
+                    }
+                }
+            }
+        }
+
+    }
+    
     public static void printInventory(String[] names, double[] prices, int[] stocks) {
         for (int i = 0; i < 10; i++) {
             if (names[i] == null || prices[i] == 0) {
